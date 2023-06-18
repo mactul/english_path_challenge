c31ce79dffc2d107ac5e21eed6bf09a7e1b234b6
mactul <mactulgames@gmail.com>
2023-06-18 20:53:47 +0200

    add a more convenient loop

--- main.c
+++ main.c
@@ -24,50 +24,53 @@ unsigned int get_distance(char* str1, char* str2)
 
 int main()
 {
-    char word[MAX_WORD_SIZE];
-    Graph* graph = graph_init();
-    GraphNode* start;
-    GraphNode* end;
-    FILE* file;
-    int word_len;
-
-    printf("Enter the starting word: ");  // the a_star algorithm find the reversed path
-    fgets(word, MAX_WORD_SIZE, stdin);
-    end = graph_add_node(graph, word, 0);
-    word_len = strlen(word);
-
-    printf("Enter the ending word: ");
-    fgets(word, MAX_WORD_SIZE, stdin);
-    if(strlen(word) != word_len)
+    while(1)
     {
-        printf("error: the starting and the ending word must have the same size\n");
-        exit(1);
-    }
-    start = graph_add_node(graph, word, get_distance(word, end->word));
+        char word[MAX_WORD_SIZE];
+        Graph* graph = graph_init();
+        GraphNode* start;
+        GraphNode* end;
+        FILE* file;
+        int word_len;
 
-    file = fopen("words_alpha.txt", "r");
-    while(fgets(word, MAX_WORD_SIZE, file) != NULL)
-    {
-        if(strlen(word) == word_len)
+        printf("Enter the starting word: ");  // the a_star algorithm find the reversed path
+        fgets(word, MAX_WORD_SIZE, stdin);
+        end = graph_add_node(graph, word, 0);
+        word_len = strlen(word);
+
+        printf("Enter the ending word  : ");
+        fgets(word, MAX_WORD_SIZE, stdin);
+        if(strlen(word) != word_len)
         {
-            graph_add_node(graph, word, get_distance(word, end->word));
+            printf("error: the starting and the ending word must have the same size\n");
+            continue;
         }
-    }
-    fclose(file);
+        start = graph_add_node(graph, word, get_distance(word, end->word));
 
-    if(!a_star(start, end))
-    {
+        file = fopen("words_alpha.txt", "r");
+        while(fgets(word, MAX_WORD_SIZE, file) != NULL)
+        {
+            if(strlen(word) == word_len)
+            {
+                graph_add_node(graph, word, get_distance(word, end->word));
+            }
+        }
+        fclose(file);
+
+        if(!a_star(start, end))
+        {
+            graph_free(&graph);
+            printf("There is no path between the starting word and the ending word\n");
+            continue;
+        }
+
+        printf("BEST PATH:\n");
+        while(end != NULL)
+        {
+            printf("%s", end->word);
+            end = end->came_from;
+        }
+
         graph_free(&graph);
-        printf("There is no path between the starting word and the ending word\n");
-        exit(0);
     }
-
-    printf("BEST PATH:\n");
-    while(end != NULL)
-    {
-        printf("%s", end->word);
-        end = end->came_from;
-    }
-
-    graph_free(&graph);
 }
